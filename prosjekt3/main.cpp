fc021b9eeb0c4dd67f3c46f804c229e2e0831853
thomashaaland <thomas.haaland@gmail.com>
2021-02-22 13:15:36 +0100

    Gauss-Legendre method moved

--- prosjekt3/main.cpp
+++ prosjekt3/main.cpp
@@ -15,6 +15,35 @@
 //using namespace std;
 std::ofstream ofile;
 
+
+double gauss_legendre(double a, double b, double *r, double *u, int n) {
+  double intgauss {0}; 
+  gauleg::gauleg(a, b, r, u, n);
+  
+  //   evaluate the integral with the Gauss-Legendre method
+  //   Note that we initialize the sum. Here brute force gauleg
+  //   with same array, x, for all directions in 6D:
+  
+  for ( int i1 = 0;  i1 < n; i1++) {
+    for ( int i2 = 0; i2 < n; i2++) {
+      for (int i3 = 0; i3 < n; i3++) {
+	for (int j1 = 0; j1 < n; j1++) {
+	  for (int j2 = 0; j2 < n; j2++) {
+	    for (int j3 = 0; j3 < n; j3++) {
+	      double term = 1.0;
+	      term *= gauleg::func_6D(r[i1],r[i2],r[i3],r[j1],r[j2],r[j3]);
+	      term *= u[i1]*u[i2]*u[i3]*u[j1]*u[j2]*u[j3];
+	      intgauss += term;
+	    }
+	  }
+	}
+      }
+    }
+  }
+  return intgauss;
+}
+
+
 //   Main function begins here
 int main()
 {
@@ -47,37 +76,20 @@ int main()
   std::cout << "File created. Looping over " << size_of_A << " values" << std::endl;
   // loop over all values of n:
   for (int l=0; l < size_of_A; l++) {
+    //inits
+    std::cout << "\r" << (int)((double)(l+1)/(size_of_A)*100) << "% complete" << std::flush;
     n = A[l];
-    // trying the 6D integral with Gauss-Legendre method:
     r = new double [n];
     u = new double [n];
     int_gauss = 0.;
     calculation_time = 0.0;
-    gauleg::gauleg(a, b, r, u, n);
-    
-    //   evaluate the integral with the Gauss-Legendre method
-    //   Note that we initialize the sum. Here brute force gauleg
-    //   with same array, x, for all directions in 6D:
     clock_t start, finish;
     start = clock();
-    std::cout << "\r" << (int)((double)(l+1)/(size_of_A)*100) << "% complete" << std::flush;
 
-    for ( int i1 = 0;  i1 < n; i1++) {
-      for ( int i2 = 0; i2 < n; i2++) {
-	for (int i3 = 0; i3 < n; i3++) {
-	  for (int j1 = 0; j1 < n; j1++) {
-	    for (int j2 = 0; j2 < n; j2++) {
-	      for (int j3 = 0; j3 < n; j3++) {
-		double term = 1.0;
-		term *= gauleg::func_6D(r[i1],r[i2],r[i3],r[j1],r[j2],r[j3]);
-		term *= u[i1]*u[i2]*u[i3]*u[j1]*u[j2]*u[j3];
-		int_gauss += term;
-	      }
-	    }
-	  }
-	}
-      }
-    }
+    
+    // trying the 6D integral with Gauss-Legendre method:
+    int_gauss = gauss_legendre(a, b, r, u, n);
+
     finish = clock();
     calculation_time = (finish - start)/(double)CLOCKS_PER_SEC;
     relatative_error = fabs(int_gauss - exact_integral)/exact_integral;
@@ -96,4 +108,5 @@ int main()
 
   return 0;
 }  // end of main program
+
 
